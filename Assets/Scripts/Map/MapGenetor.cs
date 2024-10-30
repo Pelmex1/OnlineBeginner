3f5246c38591e53e22705d515c3c22f2d995b898
Pelmex1 <89773802+Pelmex1@users.noreply.github.com>
2024-10-30 22:22:16 +0200

    Fix Spawning

--- Assets/Scripts/Map/MapGenetor.cs
+++ Assets/Scripts/Map/MapGenetor.cs
@@ -7,16 +7,24 @@ public class MapGenerator : MonoBehaviour
     [SerializeField] private GameObject _prefab;
     private float[] _positions;
     private List<float> _three_positions;
+    private int _indexX = 0;
     private void Start() {
         _positions = new float[]{_startSpawnaning.position.z - 5,_startSpawnaning.position.z, _startSpawnaning.position.z + 5};
+        for (int i = 0; i < 50; i++)
+        {
+            RandomSpawn();
+        }
     }
     private void RandomSpawn(){
         _three_positions = new List<float>(_positions);
+        int index = 0;
         for(int i = 0; i < 2; i++){
-            float randomZ = _three_positions[Random.Range(1,3)];
+            float randomZ = _three_positions[Random.Range(0,3-index)];
             _three_positions.Remove(randomZ);
-            Vector3 randomPosition = new (transform.position.x, transform.position.y, randomZ);
+            index++;
+            Vector3 randomPosition = new (transform.position.x+_indexX, _startSpawnaning.position.y, randomZ);
             Instantiate(_prefab, randomPosition, Quaternion.identity);
         }
+        _indexX += 6;
     }
 }
