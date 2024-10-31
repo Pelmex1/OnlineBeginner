5a50ed55bea8d48ab64b242d774856cc305d92d4
Pelmex1 <89773802+Pelmex1@users.noreply.github.com>
2024-10-31 18:43:51 +0200

    internet

--- Assets/Scripts/Online/LaunchSesion.cs
+++ Assets/Scripts/Online/LaunchSesion.cs
@@ -1,26 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using Photon.Pun;
 using UnityEngine;
 using Photon.Realtime;
-
+namespace OnlineBeginner.Miltiplayer{
 public class LaunchSesion : MonoBehaviourPunCallbacks
-{
-    [SerializeField] private GameObject _ErrorPanel;
-    private void Start()
     {
-        PhotonNetwork.ConnectUsingSettings();
-    }
-    public override void OnConnectedToMaster()
-    {
-        try
+        [SerializeField] private GameObject _ErrorPanel;
+        private void Start()
         {
+            PhotonNetwork.ConnectUsingSettings();
+        }
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("Succefully conected to server!");
+        }
+        public void OnClickOnMultiplayer(){
+            PhotonNetwork.AutomaticallySyncScene = true;
+            if(PhotonNetwork.IsConnected){
+                //включается панелька сетевой игры
+            } else {
+                PhotonNetwork.ConnectUsingSettings();
+                PhotonNetwork.GameVersion = StringConstants.GAME_VERSION;
+            }
+        }
+        public override void OnJoinedLobby()
+        {
+            
+        }
+        public override void OnJoinRandomFailed(short returnCode, string message)
+        {
+            //Не удалось найти комнату
+        }
+        public override void OnDisconnected(DisconnectCause cause)
+        {
+            //Когда не удалось подключится к сети
+        }
+
+        public void ConectToLobby(){
             PhotonNetwork.JoinRandomRoom();
         }
-        catch (System.Exception)
-        {
-            _ErrorPanel.SetActive(true);
+        public void CreateLobby(){
+            PhotonNetwork.CreateRoom($"{PhotonNetwork.NickName}",new RoomOptions{ MaxPlayers = 2});
+            //Включается UI лобби (без игроков)
         }
-    }
 
+    }
 }
---
+++ Assets/Scripts/StringConstants.cs
@@ -0,0 +1,5 @@
+
+public static class StringConstants
+{
+    public static readonly string GAME_VERSION = "1.0.0";
+}
