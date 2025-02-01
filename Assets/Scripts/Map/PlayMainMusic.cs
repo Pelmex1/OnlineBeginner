52d2f36adc986eaeb74033ec4da186e2bb124dc7
Rob2043 <135964485+Rob2043@users.noreply.github.com>
2025-02-01 21:38:50 +0100

    fix

--- Assets/Scripts/Map/PlayMainMusic.cs
+++ Assets/Scripts/Map/PlayMainMusic.cs
@@ -4,25 +4,37 @@ using UnityEngine;
 public class PlayMainMusic : MonoBehaviourPun
 {
     [SerializeField] private AudioSource audioSource;
+    private PhotonView photonView;
 
     private void Start()
     {
+        photonView = GetComponent<PhotonView>();
+        if (photonView == null)
+        {
+            Debug.LogError("PhotonView component is missing from this GameObject.");
+            return;
+        }
+
+        if (!photonView.IsMine && PhotonNetwork.IsConnected)
+        {
+            Debug.LogError("PhotonView is not owned by this client.");
+            return;
+        }
+
         PlayMusic();
     }
-    [PunRPC]
-    public void MainMusic() => audioSource.Play();
+
     private void PlayMusic()
     {
-        PhotonView photonView = PhotonView.Get(this);
         if (photonView != null)
         {
             photonView.RPC("MainMusic", RpcTarget.All);
         }
         else
         {
-            photonView = gameObject.AddComponent<PhotonView>();
-            PlayMusic();
-            Debug.LogWarning("PhotonView is not attached to the GameObject.");
+            Debug.LogError("PhotonView is not attached to the GameObject.");
         }
     }
+    [PunRPC]
+    public void MainMusic() => audioSource.Play();
 }
